3ac70a655a0d5dbc430c754a382bd27321f7b825
mayanjun3 <mayanjun@jd.com>
2019-05-16 18:37:50 +0800

    init

--- src/main/java/org/mayanjun/myrest/session/Session.java
+++ src/main/java/org/mayanjun/myrest/session/Session.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2018 mayanjun.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.mayanjun.myrest.session;
 
 import org.mayanjun.core.Assert;
@@ -50,6 +66,21 @@ public class Session<T> {
      * 用户数据加载器
      */
     private UserLoader<T> userLoader;
+
+    public Session(String domain, KeyPairStore keyPairStore, String tokenName, UserLoader<T> userLoader) {
+        this.domain = domain;
+        this.keyPairStore = keyPairStore;
+        this.tokenName = tokenName;
+        this.userLoader = userLoader;
+    }
+
+    public Session(String domain, KeyPairStore keyPairStore, long loginTimeout, String tokenName, UserLoader<T> userLoader) {
+        this.domain = domain;
+        this.keyPairStore = keyPairStore;
+        this.loginTimeout = loginTimeout;
+        this.tokenName = tokenName;
+        this.userLoader = userLoader;
+    }
 
     /**
      * 一个WEB请求结束后清除登录状态
