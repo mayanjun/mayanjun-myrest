3ad2a1e5b18e01ba9b590ddb02fac8b3244343bc
mayanjun3 <mayanjun@jd.com>
2019-05-16 11:09:03 +0800

    add license

--- src/main/java/org/mayanjun/myrest/interceptor/ApplicationExceptionHandler.java
+++ src/main/java/org/mayanjun/myrest/interceptor/ApplicationExceptionHandler.java
@@ -1,9 +1,21 @@
+/*
+ * Copyright 2016-2018 mayanjun.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.mayanjun.myrest.interceptor;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.mayanjun.core.ServiceException;
 import org.mayanjun.core.Status;
@@ -16,8 +28,6 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-
-import java.text.SimpleDateFormat;
 
 /**
  * Handle some exception
